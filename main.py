e694b4c5c432c34e397abc74129d72156ae6f8ab
Andre Dufresne <a_dufresne@coloradocollege.edu>
2021-01-21 14:11:37 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from flask import Flask
+from flask import request
+from flask import render_template
+from flask import redirect
+import speech_recognition as sr
+import os
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=['POST', 'GET'])
+def index():
+    transcript = ""
+    if request.method == "POST":
+        f = request.files['audio_data']
+        with open('audio.wav', 'wb') as audio:
+            f.save(audio)
+        print('file uploaded successfully')
+
+        return render_template('index.html', request="POST", )
+    else:
+        return render_template("index.html")
+
+
+@app.route("/transcribe", methods=['POST', 'GET'])
+def transcribe():
+    transcript = ""
+    if request.method == "POST":
+
+        if "file" not in request.files:
+            return redirect(request.url)
+
+        file = request.files["file"]
+        if file.filename == "":
+            return redirect(request.url)
+
+        if file:
+            recognizer = sr.Recognizer()
+            audioFile = sr.AudioFile(file)
+            with audioFile as source:
+                data = recognizer.record(source)
+            transcript = recognizer.recognize_google(data, key=None)
+
+        return render_template('transcribe.html', transcript=transcript)
+
+    else:
+        return render_template('transcribe.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
